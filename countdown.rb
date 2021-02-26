0c37fe6a9509aa501a5b1fe2853abed995b015cf
Sarah <sarahlchavez@gmail.com>
2021-02-26 06:28:44 -0800

    commit tree abb6282b565b1979b75e5603358ca7421f8d372a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(n)
+    sleep n
+end
+    
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+return "HAPPY NEW YEAR!"
 end
